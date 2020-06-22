9ded56cd944baaa2cfbbb8ed84af17c1ba9b57f0
hideyosh1 <64223175+hideyosh1@users.noreply.github.com>
2020-06-21 20:57:17 -0500

    sentencemaker 1.2 (c++)
    hooray! i got it done in like 10 mins

--- Source.cpp
+++ Source.cpp
@@ -14,6 +14,9 @@ int main(void) {
 		std::cout << "note that only 200 words can be used due to limitations.\n";
 		std::vector<std::string>words;
 		words.reserve(200);
+		char punc;
+		std::cout << "would you like an:\n exclamation mark (e),\n a period (p),\n a question mark (q),\n or would you like to skip this step?(any other key)";
+		std::cin >> punc;
 		for (int i = 0; i < numword; i++) {
 			std::string temp;
 			std::cout << "enter word: \n";
@@ -23,6 +26,19 @@ int main(void) {
 		}for (int i = 0; i < numword; i++) {
 			std::cout << words[i] << " ";
 		}
-
+		if (punc == 'e') {
+			std::cout << "\b!\n";
+		}
+		else if (punc == 'q') {
+			std::cout << "\b?\n";
+		}
+		else if (punc == 'p') {
+			std::cout << "\b.\n";
+		}
+		else {
+			std::cout << "\b";
+		}
+		
 	}
+	return 0;
 }
